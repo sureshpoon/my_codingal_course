266fd3f046adcfc517d0e56efde8236f8b21e87c
sureshpoon <sureshpun2@gmail.com>
2025-04-29 15:02:03 +0545

    hw 6

---
+++ Assignments/hw6.py
@@ -0,0 +1,9 @@
+character= input("Enter any one character : ") 
+if character == "A" or character == "B" or character == "C" or character == "D" or character == "E" or character == "F" or character == "G" or character == "H" or character == "I" or character =="J" or character == "K" or character == "L" or character == "M" or character == "N" or character == "O" or character == "P" or character == "Q" or character == "R" or character == "S" or character == "T" or character == "U" or character == "V" or character == "W" or character == "X" or character == "Y" or character == "Z":
+    print (f"{character} is an alphabet") 
+
+elif character =="a" or character == "b" or character == "c" or character == "d" or character == "e" or character == "f" or character == "g" or character == "h" or character == "i" or character =="j" or character == "k" or character == "l" or character == "m" or character == "n" or character == "o" or character == "p" or character == "q" or character == "r" or character == "s" or character == "t" or character == "u" or character == "v" or character == "w" or character == "x" or character == "y" or character == "z":
+    print (f"{character} is an alphabet")
+
+else: 
+    print (f"{character} is not an alphabet")   
