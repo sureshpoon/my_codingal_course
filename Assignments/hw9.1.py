8e2a5745d043c1547eea2cd3913fbf93bcf34dc6
sureshpoon <sureshpun2@gmail.com>
2025-05-05 18:02:30 +0545

    hw 9

---
+++ Assignments/hw9.1.py
@@ -0,0 +1,7 @@
+a = int (input ("Enter any number : "))
+b = int (input ("Enter another number : "))
+print ("Before swapping, a=", a, "b=",b)
+temp = a
+a = b
+b = temp
+print ("After swapping, a=", a, "b=",b)
---
+++ Assignments/hw9.py
@@ -0,0 +1,7 @@
+a = int (input ("Enter any number : "))
+b = int (input ("Enter another number : "))
+print ("Before swapping, a=", a, "b=",b)
+a= a+b
+b = a-b
+a = a - b
+print ("After swapping, a=", a, "b=",b)
